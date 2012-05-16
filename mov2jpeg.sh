7fa8d3570206455c142e01d786cdc896b21f0b3f
Chris Harding <chris.harding@acm.org>
2012-05-16 16:22:41 +0200

    Video script working for single vidoe

--- mov2jpeg.sh
+++ mov2jpeg.sh
@@ -40,8 +40,8 @@ echo "Output dimensions: $DST_W x $DST_H"
 echo "Output framerate: $DST_FPS fps"
 
 # Get video dimensions #
-SRC_W=`ffprobe $SRC_FILE |& grep -o '[0-9]\{2,4\}x[0-9]\{2,4\}' | grep -o '[0-9]\{1,4\}x' | sed 's/x//'`
-SRC_H=`ffprobe $SRC_FILE |& grep -o '[0-9]\{2,4\}x[0-9]\{2,4\}' | grep -o 'x[0-9]\{1,4\}' | sed 's/x//'`
+SRC_W=`ffprobe $SRC_FILE 2>&1 | grep -o '[0-9]\{2,4\}x[0-9]\{2,4\}' | grep -o '[0-9]\{1,4\}x' | sed 's/x//'`
+SRC_H=`ffprobe $SRC_FILE 2>&1 | grep -o '[0-9]\{2,4\}x[0-9]\{2,4\}' | grep -o 'x[0-9]\{1,4\}' | sed 's/x//'`
 echo "Input dimensions: $SRC_W x $SRC_H"
 
 # Initially set crop to entire source #
@@ -52,14 +52,14 @@ CROP_H=$SRC_H
 if [ $SRC_W -lt $SRC_H ]; then
 
     # If aspect ratio too tall #
-    if [ $($SRC_W / $SRC_H) -lt $($DST_W / $DST_H) ]; then
-	CROP_H=$( ($DST_H / $DST_W) * $CROP_W )
+    if [ $(($SRC_H / $SRC_W)) -lt $(($DST_H / $DST_W)) ]; then
+	CROP_W=$(( ($DST_W * $CROP_H) / $DST_H ))
     # If aspect ratio too wide #
-    elif [ $($SRC_W / $SRC_H) -gt $($DST_W / $DST_H) ]; then
-	CROP_W=$( ($DST_W / $DST_H) * $CROP_H )
+    elif [ $(($SRC_H / $SRC_W)) -gt $(($DST_H / $DST_W)) ]; then
+	CROP_H=$(( ($DST_H * $CROP_W ) / $DST_W ))
     fi 
 
-    ffmpeg -i -vf "crop=${CROP_W}:${CROP_H},scale=${DST_W}:${DST_H}" $SRC_FILE -s $DST_Wx$DST_H -an -f image2 -qmax 1 -qmin 1 "${DST_PATH}/frame_%05d.jpg"
+    ffmpeg -i $SRC_FILE -vf "crop=${CROP_H}:${CROP_W},scale=${DST_H}:${DST_W}" -r $DST_FPS  -an -f image2 -qmax 1 -qmin 1 "${DST_PATH}/frame_%05d.jpg"
 
 else
 
@@ -71,7 +71,7 @@ else
 	CROP_W=$(( ($DST_W * $CROP_H ) / $DST_H ))
     fi 
 
-    ffmpeg -i $SRC_FILE -vf "crop=${CROP_W}:${CROP_H},scale=${DST_W}:${DST_H}" -an -f image2 -qmax 1 -qmin 1 "${DST_PATH}/frame_%05d.jpg"
+    ffmpeg -i $SRC_FILE -vf "crop=${CROP_W}:${CROP_H},scale=${DST_W}:${DST_H}" -r $DST_FPS -an -f image2 -qmax 1 -qmin 1 "${DST_PATH}/frame_%05d.jpg"
 
 fi
 
