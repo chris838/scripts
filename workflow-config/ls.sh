564f117e2504816ab84bfb4f6f5ef8a34e59679a
Chris Harding <chrish@cantab.net>
2015-04-18 12:03:51 +0100

    Added ls aliases, included coloured output.

---
+++ workflow-config/ls.sh
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------
+# The 'ls' family
+#-------------------------------------------------------------
+# Add colors for filetype and  human-readable sizes by default on 'ls':
+alias ls='ls -h -G'
+alias lx='ls -lXB'         #  Sort by extension.
+alias lk='ls -lSr'         #  Sort by size, biggest last.
+alias lt='ls -ltr'         #  Sort by date, most recent last.
+alias lc='ls -ltcr'        #  Sort by/show change time,most recent last.
+alias lu='ls -ltur'        #  Sort by/show access time,most recent last.
+
+# The ubiquitous 'll'
+alias ll="ls -lv"
+alias lm='ll |more'        #  Pipe through 'more'
+alias lr='ll -R'           #  Recursive ls.
+alias la='ll -A'           #  Show hidden files.
+alias tree='tree -Csuh'    #  Nice alternative to 'recursive ls' ...
