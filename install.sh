882c0cfc57a6d5e18d83eacff07d2ac8d7bc275d
Chris Harding <charding@palantir.com>
2015-07-03 11:14:21 +0100

    Moved sublime link to install script.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 echo "Installing Mac OS X key bindings"
 mv -n ~/Library/KeyBindings/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict.BACKUP
 rm ~/Library/KeyBindings/DefaultKeyBinding.dict
+mkdir ~/Library/KeyBindings
 ln -s ~/scripts/workflow-config/DefaultKeyBinding.dict ~/Library/KeyBindings/DefaultKeyBinding.dict 
 
 echo "Installing shell customisations"
@@ -10,6 +11,9 @@ mv -n ~/.bash_profile ~/.bash_profile.BACKUP
 rm ~/.bash_profile
 ln -s ~/scripts/workflow-config/bash_profile ~/.bash_profile
 source ~/.bash_profile
+
+echo "Enable opening sublime from the command line"
+ln -s /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 
 echo "Installing Karibiner bindings"
 mv -n ~/Library/Application\ Support/Karabiner/private.xml ~/Library/Application\ Support/Karabiner/private.xml.BACKUP
